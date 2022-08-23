7944393c5bb849167f296b572d8d9272def420a5
Engineer-Q <ansarisohail1995@gmail.com>
2022-08-23 13:12:16 +0530

    box created

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-console.log('hello world')
+document.addEventListener("DOMContentLoaded", function () {
+    createBox(32);
+})
+
+function createBox(size) {
+    const board = document.querySelector('.board')
+    board.style.gridTemplateColumns = `repeat (${size} , 1fr)`
+    board.style.gridTemplateRows = `repeat (${size} , 1fr)`
+    let numDiv = size * size
+
+    for (let i = 0; i < numDiv; i++) {
+        let div = document.createElement('div');
+        div.style.backgroundColor = 'yellow'
+        board.insertAdjacentElement("beforeend", div);
+    }
+}
+
+function color () {
+    board.addEventListener("mouseover", function(){
+        div.style.backgroundColor = 'black'
+    })
+}
+
+
